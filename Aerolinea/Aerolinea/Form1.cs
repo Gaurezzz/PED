80e4318d60649d519a1f6847709502b2eaf4f0f3
LuisDC2001 <luisdelcid248@gmail.com>
2022-03-09 16:34:02 -0600

    Form

--- Aerolinea/Aerolinea/Form1.cs
+++ Aerolinea/Aerolinea/Form1.cs
@@ -16,6 +16,5 @@ namespace Aerolinea
         {
             InitializeComponent();
         }
-
     }
 }
--- Aerolinea/Aerolinea/Program.cs
+++ Aerolinea/Aerolinea/Program.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -6,15 +6,14 @@ using System.Windows.Forms;
 
 namespace Aerolinea
 {
-    internal static class Program
+    static class Program
     {
         /// <summary>
-        ///  The main entry point for the application.
+        /// Punto de entrada principal para la aplicación.
         /// </summary>
         [STAThread]
         static void Main()
         {
-            Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
